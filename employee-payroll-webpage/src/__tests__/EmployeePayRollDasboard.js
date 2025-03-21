669cba87ce8ee22f40fd150e392be1b23e37e74e
Shruticdh <shruticdh2003@gmail.com>
2025-03-21 11:10:58 +0530

    [Shruti] Added: wrote test case for image and text in navbar

--- employee-payroll-webpage/src/__tests__/EmployeePayRollDasboard.js
+++ employee-payroll-webpage/src/__tests__/EmployeePayRollDasboard.js
@@ -6,3 +6,13 @@ test('renders hello world' , () =>{
     const linkElement = screen.getByText(/hello world/i);
     expect(linkElement).toBeInTheDocument();
 });
+
+test("Image should render properly", () => {
+    render(<NavBar />);
+    const imgElement = screen.getByAltText("Company Logo"); 
+    expect(imgElement).toBeInTheDocument();
+    expect(imgElement).toHaveAttribute("src", "logo.png"); 
+
+    const linkElement = screen.getByText(/EMPLOYEE PAYROLL/i);
+    expect(linkElement).toBeInTheDocument();
+  });
